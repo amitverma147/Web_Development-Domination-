2a8fb1fc9aea62df7b7cc08a16c5489eba332ad2
amitverma147 <vamit20000@gmail.com>
2024-05-06 19:59:06 +0530

    learning react

--- React_project/src/components/Card.jsx
+++ React_project/src/components/Card.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 
 function Card() {
+    var data=[{image:'https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name:"Amazon Basics", description:"loremsdhguashuhushuiahiuhsfduihgufhsduhvng"},
+    {image:'https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name:"Amazon Basics", description:"loremsdhguashuhushuiahiuhsfduihgufhsduhvng"},
+    {image:'https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name:"Amazon Basics", description:"loremsdhguashuhushuiahiuhsfduihgufhsduhvng"},
+
+    ]
+
   return (
-    <div className='w-full h-screen bg-zinc-200'>
-        <div className=' w-52 bg-zinc-100 absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] rounded-md overflow-hidden '>  
+    <div className='w-full h-screen bg-zinc-200 flex items-center justify-center gap-10'>
+       {data.map((elem,index)=>(
+         <div key={index} className=' w-52 bg-zinc-100 rounded-md overflow-hidden '>  
 
            <div className='w-full h-32 bg-zinc-300 '>
-            <img className='w-full h-full object-over object-center' src="https://images.unsplash.com/photo-1522780550166-284a0288c8df?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img className='w-full h-full object-cover object-center' src={elem.image} alt="" />
            </div>
             <div className='w-full px-3 py-4'>
-                <h2 className='font-semibold'>Amazon Basics</h2>
-                <p className='text-xs mt-5'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa neque enim odit.</p>
+                <h2 className='font-semibold'>{elem.name}</h2>
+                <p className='text-xs mt-5'>{elem.description}</p>
             </div>
         </div>
+       ))}
     </div>
-  )
+  );
 }
 
 export default Card
