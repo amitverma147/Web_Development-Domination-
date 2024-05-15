0d4ff2c6e6ccecd491b887a0e31b9ca7d57943fb
amitverma147 <vamit20000@gmail.com>
2024-05-15 23:10:52 +0530

    #

--- React_project/src/App.jsx
+++ React_project/src/App.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './components/Card'
 
-
-
 function App() {
+ const data=[
+    {name:"AMIT",profession:"Developer",image:"https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",friends:false},
+    {name:"RAHUL",profession:"ACTIVIST",image:"https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",friends:false},
+    {name:"AAYUSH",profession:"BACKCHOD",image:"https://images.unsplash.com/photo-1480429370139-e0132c086e2a?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",friends:false},
+    {name:"AMARDEEP",profession:"HUMAN",image:"https://plus.unsplash.com/premium_photo-1664533227571-cb18551cac82?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",friends:false},
+ ]
 
-  
+  const[Val,setVal]=useState(data);
 
+  const handleFriendButton=(changingIndex)=>{
+    setVal((previous) => {
+  return previous.map((item, index) => {
+    if (index === changingIndex) {
+      // Spread the item first to ensure immutability
+      return { ...item, friends: !item.friends };
+    }
+    return item;
+  });
+});
+  }
 
   return (
     <>
-    <Card/>
+    <div className='w-full h-screen bg-zinc-300 flex gap-4 items-center justify-center'>
+      {Val.map(( item,index)=>(
+      <Card key={index} index={index} values={item} handleClick={handleFriendButton} />
+      ))}
+    </div>
+   
     </>
   )
 }
--- React_project/src/components/Card.jsx
+++ React_project/src/components/Card.jsx
@@ -1,29 +1,19 @@
 import React from 'react'
 
-function Card() {
-
-      const data=[{image :'https://images.unsplash.com/photo-1633174524778-61a18ee54490?q=80&w=1796&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', heading:"Amazon Basics", description :"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est veritatis cum aspernatur?", stock: true  },
-    {
-        image:'https://images.unsplash.com/photo-1654573817889-296cad084c97?q=80&w=2062&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', heading:"Flipkart",description :"Lorem, ipsum dolor sit amet consect", stock:false
-    },
-    ];
-
-
+function Card({values,handleClick}) {
+  const{name,index,image,profession,friends}=values;
   return (
-    <div className='w-full h-screen bg-zinc-200 flex items-center justify-center gap-10'>
-       {data.map((elem,index)=>(
-         <div key={index} className='w-52 bg-zinc-100  rounded-md overflow-hidden' >
-            <div className='w-full h-32 bg-zinc-300 '>
-                <img className='w-full h-full object-cover'  src={elem.image} alt="" />
-            </div>
-            <div className='w-full px-3 py-4'>
-                <h2 className='font-semibold'>{elem.heading}</h2>
-                <p className='text-xs mt-5'>{elem.description}</p>
-                <button className={`text-xs px-4 py-1 ${elem.stock?'bg-sky-600' :'bg-red-600'} rounded mt-3`}>{elem.stock ? "In stock" :"Out of Stock"}</button>
-            </div>
+    <div className='w-52 bg-white-200 rounded-md overflow-hidden'>
+      <div className='w-full h-32 bg-sky-200'>
+        <img className='w-full h-full object-cover' src= {image} alt="" />
+      </div>
+        <div className='w-full p-3 bg-white'>
+          <h3 className='text-xl font-semibold'>{name}</h3>
+          <h5 className='text-xs'>{profession}</h5>
+          <button onClick={()=>handleClick(index)} className='px-3 py-1 text-xs text-white bg-blue-500 font-semibold rounded-md'>
+            {friends === true ? "Friends" : "Add Friend" }</button>
         </div>
-       ))} 
-    </div>
+      </div>
   )
 }
 
